823ac64b46febaa1f9271d78e8853fef9e7ce53e
sai <sai.bhavanikumae6393@gmail.com>
2017-02-10 15:47:36 +0530

    initial comit

---
+++ app/src/main/java/com/example/sai/bank/SuccessActivity.java
@@ -0,0 +1,21 @@
+package com.example.sai.bank;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+
+    public void home(View view) {
+        Intent home_intent;
+        home_intent=new Intent(SuccessActivity.this,homeActivity.class);
+        startActivity(home_intent);
+    }
+}
